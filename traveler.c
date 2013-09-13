956b73cdcceb901de43a42e7249d906f3588ed48
betoluna <betoluna1@gmail.com>
2013-09-13 15:57:05 -0400

    move functions after main in develop

--- traveler.c
+++ traveler.c
@@ -1,7 +1,6 @@
 /* type a single string on a line containig a set of state abbreviations 
    separated by spaces, print them in order one per line, compute the 
    percentage of the US visited */
-
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
@@ -14,72 +13,6 @@ struct node {
   char *value;
   struct node *next;
 };
-
-// insert string in place into sorted list
-struct node *insert(struct node *head, char *new_val) {
-  // if list is empty 
-  if(head == NULL) {
-    head = malloc(sizeof(struct node)); // check for NULL after call to malloc
-    head->value = new_val;
-    head->next = NULL;
-    return head;
-  } 
-
-  // if incoming val is less than val at head, insert at front.    
-  if(strcmp(new_val, head->value) < 0) {
-    struct node *new_head = malloc(sizeof(struct node));  //check for NULL afterward
-    new_head->value = new_val;
-    new_head->next = head;
-    return new_head;
-  }
-
-  // must traverse list to find proper location to insert
-  struct node *current = head;
-  struct node *previous = NULL;
-  // while new_val is greater than value pointed to by current
-  while(current != NULL && strcmp(current->value, new_val) <= 0) {
-    // Don't allow duplicates. If new_val is found, don't insert.
-    if(strcmp(current->value, new_val) == 0)
-      return;
-
-    previous = current;
-    current = current->next;
-  }
-
-  // insert when proper position found
-  struct node *new_node = malloc(sizeof(struct node));
-  new_node->value = new_val;
-  new_node->next = current; // may or may not be NULL
-  previous->next = new_node; // insert after previous
-  
-  return head;
-} // END insert()
-
-static float percent(int visited) {
-  return (float)visited / STATES * 100;
-}
-
-void print(struct node *head) {
-  struct node *temp = head;
-  int visited = 0;
-  char c, *val;
-  while(temp != NULL) {
-    val = temp->value;
-    while(*val) {
-      c = *val;
-      if(islower(c))
-	c = toupper(c);
-      putchar(c);
-      *val++;
-    }
-    temp = temp->next;
-    visited++;
-    printf("\n");
-  }
- 
-  float percentVisited = percent(visited);
-  printf("You have visited %.0f%% of the United States.\n", percentVisited);
-}
     
 int main(int argc, char **argv) {
   char buf[INPUTSIZE]; // the incoming string from stdin
@@ -106,4 +39,71 @@ int main(int argc, char **argv) {
   print(head);
 
   return 0;
+} // end main
+
+// insert string --if it's not already there, into sorted list
+struct node *insert(struct node *head, char *new_val) {
+  // if list is empty 
+  if(head == NULL) {
+    head = malloc(sizeof(struct node)); // check for NULL after call to malloc
+    head->value = new_val;
+    head->next = NULL;
+    return head;
+  } 
+
+  // if incoming val is less than val at head, insert at front.    
+  if(strcmp(new_val, head->value) < 0) {
+    struct node *new_head = malloc(sizeof(struct node));  //check for NULL afterward
+    new_head->value = new_val;
+    new_head->next = head;
+    return new_head;
+  }
+
+  // must traverse list to find proper location to insert
+  struct node *current = head;
+  struct node *previous = NULL;
+  // while new_val is greater than value pointed to by current
+  while(current != NULL && strcmp(current->value, new_val) <= 0) {
+    // Don't allow duplicates. If new_val is found, do nothing.
+    if(strcmp(current->value, new_val) == 0)
+      return;
+
+    previous = current;
+    current = current->next;
+  }
+
+  // insert when proper position found
+  struct node *new_node = malloc(sizeof(struct node));
+  new_node->value = new_val;
+  new_node->next = current; // may or may not be NULL
+  previous->next = new_node; // insert after previous
+  
+  return head;
+} // END insert()
+
+void print(struct node *head) {
+  struct node *temp = head;
+  int visited = 0;
+  char c, *val;
+  while(temp != NULL) {
+    val = temp->value;
+    while(*val) {
+      c = *val;
+      if(islower(c))
+	c = toupper(c);
+      putchar(c);
+      *val++;
+    }
+    temp = temp->next;
+    visited++;
+    printf("\n");
+  }
+ 
+  float percentVisited = percent(visited);
+  printf("You have visited %.0f%% of the United States.\n", percentVisited);
 }
+
+static float percent(int visited) {
+  return (float)visited / STATES * 100;
+}
+
